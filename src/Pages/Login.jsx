9843ff8f9ce1504fceda845d54f938ecb59dd62d
farrahfahira <farrahfahira2202@gmail.com>
2022-11-18 12:25:05 +0700

    commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,20 @@
 import "../assets/css/Login.css";
+import loginimg from "../assets/image/login.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="Login">
-      <div className="section-image">ini image</div>
+      <div className="section-image">
+        <img src={loginimg} alt="" />
+      </div>
       <div className="section-form">
         <div className="signup">
           <h4>dont have an account?</h4>
           <button>
-            <span>Sign Up</span>
+            <Link to="/signup">
+              <span>Sign Up</span>
+            </Link>
           </button>
         </div>
         <div className="login-form">
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,7 +1,47 @@
 import "../assets/css/SignUp.css";
+import loginimg from "../assets/image/signup.svg";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div className="Signup">ini signup</div>;
+  return (
+    <div className="Signup">
+      <div className="login">
+        <h4>already have an account?</h4>
+        <button>
+          <Link to="/login">
+            <span>Login</span>
+          </Link>
+        </button>
+      </div>
+      <div className="signup-section">
+        <div className="signup-form">
+          <div className="signup-title">
+            <h2>Sign Up</h2>
+          </div>
+          <div className="signup-input">
+            <div className="input-element">
+              <input type="text" placeholder="email" />
+            </div>
+            <div className="input-element">
+              <input type="text" placeholder="phone" />
+            </div>
+            <div className="input-element">
+              <input type="text" placeholder="password" />
+            </div>
+            <div className="input-element">
+              <input type="text" placeholder="confirm password" />
+            </div>
+            <div className="input-element-button">
+              <button>Sign Up</button>
+            </div>
+          </div>
+        </div>
+        <div className="signup-img">
+          <img src={loginimg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
