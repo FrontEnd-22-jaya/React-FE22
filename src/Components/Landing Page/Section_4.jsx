3e91371e1af9a8939d4db8fa6922107bd6b407de
farrahfahira <farrahfahira2202@gmail.com>
2022-11-20 18:08:16 +0700

    commit

--- src/Components/Landing Page/Section_4.jsx
+++ src/Components/Landing Page/Section_4.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import Card from '../Sec_4/Card'
+import React from "react";
+import Card from "../Sec_4/Card";
 
 const Section_4 = () => {
   return (
-    <>
-    <div className="saysJob">
-    <div className="saysJob-text">
-      <h3>What Says Job Seeker About Us</h3>
-      <p>Various positive reviews from workers at Jobsidian</p>
-    </div>
-    </div>
-    <div className="main-card">
-        <div className="cards">
-            <Card/>
-            
-            
+    <div className="section4">
+      <div className="saysJob">
+        <div className="saysJob-text">
+          <h3>What Job Seeker Says About Us</h3>
+          <p>Various positive reviews from workers at Jobsidian</p>
         </div>
-               <div className="dots">
-                <div type="radio" name="dot" id="one"></div>
-                <div type="radio" name="dot" id="two"></div>
-                <div type="radio" name="dot" id="three"></div>
-               </div>
-
-              <div className="button">
-                <label for="one" className="active one"></label>
-                <label for="two" className="two"></label>
-              </div>
       </div>
-    </>
-  )
-}
+      <div className="main-card">
+        <div className="cards">
+          <Card />
+        </div>
+        <div className="cards">
+          <Card />
+        </div>
+        <div className="cards">
+          <Card />
+        </div>
+        <div className="dots">
+          <div type="radio" name="dot" id="one"></div>
+          <div type="radio" name="dot" id="two"></div>
+          <div type="radio" name="dot" id="three"></div>
+        </div>
 
-export default Section_4
+        <div className="button">
+          <label for="one" className="active one"></label>
+          <label for="two" className="two"></label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section_4;
