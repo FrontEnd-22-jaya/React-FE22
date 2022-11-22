d297979da122a960fbf011cdfa1aae4f20d1096f
farrahfahira <farrahfahira2202@gmail.com>
2022-11-22 17:10:16 +0700

    commit

--- src/Pages/ProfilePage/ProfilPage.jsx
+++ src/Pages/ProfilePage/ProfilPage.jsx
@@ -23,7 +23,40 @@ const ProfilPage = () => {
               <img src={pfp} alt="" />
               <span>Nama User</span>
             </div>
-            <div className="profile-info"></div>
+            <div className="profile-info">
+              <div className="profile-info-left">
+                <ul>
+                  <li>
+                    <label htmlFor="phone-number">Phone Number</label>
+                    <span>123456789</span>
+                  </li>
+                  <li>
+                    <label htmlFor="gender">Gender</label>
+                    <span>Female</span>
+                  </li>
+                  <li>
+                    <label htmlFor="birthdate">Date of Birth</label>
+                    <span>22 Februari 2002</span>
+                  </li>
+                </ul>
+              </div>
+              <div className="profile-info-right">
+                <ul>
+                  <li>
+                    <label htmlFor="city">City</label>
+                    <span>Lampung</span>
+                  </li>
+                  <li>
+                    <label htmlFor="address">Address</label>
+                    <span>Jl. Raya Lampung</span>
+                  </li>
+                  <li>
+                    <label htmlFor="email">Email</label>
+                    <span>user@gmail.com</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
             <div className="detil-info"></div>
           </div>
         </div>
